e4e4f24a0ba5ed832e613742465c8729fa337ef7
GurshaanK <kang501@purdue.edu>
2024-03-10 13:44:54 -0400

    Worked on allowing Users to delete their accounts, ran into problems with axios.delete

--- frontend/src/components/settings.js
+++ frontend/src/components/settings.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
+import axios from "axios";
 import SecondaryBanner from "./SecondaryBanner";
 import SecondaryBottomBar from "./SecondaryBottomBar";
 import "../App.css";
@@ -10,6 +11,7 @@ const Settings = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const userId = localStorage.getItem("userId");
 
   const handleSubmit = () => {
     // Implement your submission logic here
@@ -18,33 +20,49 @@ const Settings = () => {
     console.log("Password:", password);
   };
 
+  // Delete does not work in backend
+
+  const handleDelete = async () => {
+    if (window.confirm("Are you sure you want to delete your Account?")) {
+      // User clicked OK, perform the delete operation
+      console.log("Deleting user with ID:", userId);
+      // axios.delete(`http://localhost:5001/user/${userId}`)
+      //   .then(navigate("/"))
+      //   .catch((error) => {
+      //     console.error("Error:", error);
+      //   });
+    }
+  }
+
   return (
     <div>
       <SecondaryBanner />
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <h1 style={{ fontWeight: "bold" }}>User's Full Name</h1>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={{ marginRight: "10px", padding: "5px" }}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ padding: "5px" }}
-        />
-      </div>
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <button onClick={handleSubmit} style={{ padding: "10px 20px", fontSize: "16px" }}>Submit</button>
-      </div>
-      <div>
-        <button>Delete Account</button>
+      <div className="settings-container">
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <h1 style={{ fontWeight: "bold" }}>User's Full Name</h1>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ marginRight: "10px", padding: "5px" }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ padding: "5px" }}
+          />
+        </div>
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+          <button onClick={handleSubmit} style={{ padding: "10px 20px", fontSize: "16px" }}>Submit</button>
+        </div>
+        <div>
+          <button className="delete-button" onClick={handleDelete}>Delete Account</button>
+        </div>
       </div>
       <SecondaryBottomBar />
     </div>
