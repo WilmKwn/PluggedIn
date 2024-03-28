4f7eb603ee000488ea3e385667fe6b9e1e6b92e0
Kanishk Kamalvanshi <kkamalvanshi@gmail.com>
2024-03-27 21:09:52 -0400

    Initial SignUp Page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,36 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-
-import { auth } from "./firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+// You might need additional imports depending on the rest of your app's functionalities
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import "../App.css";
+import "../App.css"; // Ensure the path matches your project structure
+
 library.add(faSearch);
 
 const App = () => {
   const navigate = useNavigate();
 
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const userId = auth.currentUser.uid;
-        const actualUserIdBecauseWilliamYongUkKwonIsAnnoying = auth.currentUser.uid;
-        localStorage.setItem("userId", userId);
-        localStorage.setItem("actualUserIdBecauseWilliamYongUkKwonIsAnnoying", userId);
-
-        axios.get(`http://localhost:5001/user/${userId}`).then((res) => {
-          navigate("/feed");
-        }).catch((err) => {
-          navigate("/onboarding");
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+  // Function to navigate to the sign-in page
+  const redirectToSignInPage = () => {
+    navigate("/signup");
   };
 
   return (
@@ -38,7 +20,7 @@ const App = () => {
       <header className="app-header">
         <div>PluggedIn</div>
         <nav>
-          <button onClick={signInWithGoogle}>Sign In</button>
+          <button onClick={redirectToSignInPage}>Sign In</button>
         </nav>
       </header>
 
@@ -46,6 +28,7 @@ const App = () => {
         <div>Welcome to PluggedIn</div>
         <div>Connect. Collab. Become a Rockstar!</div>
       </main>
+
       <footer className="app-footer">
         <div>CS 307 Team 40</div>
       </footer>
