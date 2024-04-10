a3d520532b993aa023e73553e07e2aec969f93ad
FraAurCas <frankcasella17@gmail.com>
2024-04-09 23:06:58 -0400

    Can get gifs
    Search for number 1 gif with search text in message box

--- frontend/src/components/Messaging.js
+++ frontend/src/components/Messaging.js
@@ -52,7 +52,7 @@ const Messaging = ({ title }) => {
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
-
+  const [gifModalOpen, setGifModalOpen] = useState(false);
   const findConversation = () => {
     const user1id = userId < currFriendObj.uid ? userId : currFriendObj.uid;
     const user2id = userId > currFriendObj.uid ? userId : currFriendObj.uid;
@@ -125,6 +125,81 @@ const Messaging = ({ title }) => {
     setCurrFriendObj({ uid: currFriendObj.uid, realname: currFriendObj.realname });
     findConversation();
   };
+  //AIzaSyDQ5G0U8_26WdvFn-_l5EAQ9BDcATMiKEs 
+  const handleGif = () => {
+    grab_data();
+    setGifModalOpen(true);
+  }
+  const closeGifModal = () => {
+    // Close the GIF modal
+    setGifModalOpen(false);
+  };
+  function httpGetAsync(theUrl, callback)
+{
+    // create the request object
+    var xmlHttp = new XMLHttpRequest();
+
+    // set the state change callback to capture when the response comes in
+    xmlHttp.onreadystatechange = function()
+    {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    // open as a GET call, pass in the url and set async = True
+    xmlHttp.open("GET", theUrl, true);
+
+    // call send with no params as they were passed in on the url string
+    xmlHttp.send(null);
+
+    return;
+}
+function tenorCallback_share(responsetext)
+{
+    // no action is needed in the share callback
+}
+// callback for the top 8 GIFs of search
+function tenorCallback_search(responsetext)
+{
+    // Parse the JSON response
+    var response_objects = JSON.parse(responsetext);
+
+    var top_10_gifs = response_objects["results"];
+
+    // load the GIFs -- for our example we will load the first GIFs preview size (nanogif) and share size (gif)
+
+    document.getElementById("preview_gif").src = top_10_gifs[0]["media_formats"]["nanogif"]["url"];
+
+    document.getElementById("share_gif").src = top_10_gifs[0]["media_formats"]["gif"]["url"];
+
+    return;
+
+}
+
+
+// function to call the trending and category endpoints
+function grab_data()
+{
+    // set the apikey and limit
+    var apikey = "AIzaSyDQ5G0U8_26WdvFn-_l5EAQ9BDcATMiKEs";
+    var clientkey = "my_test_app";
+    var lmt = 8;
+
+    // test search term
+    var search_term = messageText;
+
+    // using default locale of en_US
+    var search_url = "https://tenor.googleapis.com/v2/search?q=" + search_term + "&key=" +
+            apikey +"&client_key=" + clientkey +  "&limit=" + lmt;
+
+    httpGetAsync(search_url,tenorCallback_search);
+
+    // data will be loaded by each call's callback
+    return;
+}
+
   const modalStyles = {
     content: {
       position: 'absolute',
@@ -251,6 +326,26 @@ const Messaging = ({ title }) => {
               <button onClick={handleSendMessage} className="button rounded-r-md ml-2 mb-1">
                 <FontAwesomeIcon icon={faPaperPlane} />
               </button>
+              <button onClick={handleGif} className="button rounded-r-md ml-2 mb-1">
+                gif
+              </button>
+              {gifModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-4 rounded-md">
+          <h2># 1 GIF loaded - preview image</h2>
+<img id="preview_gif" src="" alt=""></img>
+
+<h2># 1 GIF loaded - share image</h2>
+<img id="share_gif" src="" alt=""></img>
+
+            <button onClick={closeGifModal} className="button mt-4">
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
+
             </div>
           </div>
         </div>
